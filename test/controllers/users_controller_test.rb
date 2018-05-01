ae9142f85f0204db2b6d18de9dd70203e5f1de04
Michael Lamb <mflambjr@gmail.com>
2018-05-01 18:27:16 -0400

    Fixing failed CI test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get users_index_url
+    get root_url
     assert_response :success
   end
 
