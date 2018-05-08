818c17a600738f2f2189368a083b64029474cc92
Jordan Hollinger <jhollinger2@gmail.com>
2018-05-07 21:58:25 -0400

    Add controller for organizations to provide map data and pop-up content

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
@@ -8,6 +7,8 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   get '/map', to: 'map#index'
+
+  resources :organizations, only: [:index, :show]
 
   resources :users, only: [:new, :create, :index]
 
---
+++ test/controllers/organizations_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class OrganizationsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get organizations_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get organizations_show_url
+    assert_response :success
+  end
+
+end
