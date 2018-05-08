dd3c5366cd6769729739be2ed5e60001937a2143
Jonathan Mast <jon@jonmast.com>
2018-05-08 19:38:19 -0400

    Add boilerplate category to seeds to pass validation
    Also use create! so the script fails when validations aren't satisfied.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,11 @@
-sarnelli = Organization.create(
+# Wipe everything clean first
+Need.destroy_all
+Category.destroy_all
+Organization.destroy_all
+
+category = Category.create!(name: 'uncategorized')
+
+sarnelli = Organization.create!(
   name: 'Blessed Sarnelli Community',
   description: 'Blessed Sarnelli Community is an organization dedicated to providing for the needs of people who are poor and abandoned with an emphasis on empowering young adults to share in this commitment through prayer, hospitality and service.',
   address: '2739 Kensington Ave, Philadelphia, PA 19134',
@@ -6,43 +13,47 @@ sarnelli = Organization.create(
 )
 
 sarnelli_needs_list =  [
-  'white socks', 
-  'mens underwear', 
-  't-shirts', 
-  'bars of soap', 
-  'pants', 
-  'long underwear', 
-  'shoes', 
-  'sweaters', 
-  'jackets', 
-  'shaving cream', 
-  'razors', 
-  'shampoo', 
-  'conditioner', 
-  'body wash', 
-  'toothbrushes', 
-  'toothpaste', 
-  'lotion', 
-  'hair brushes', 
-  'nail clippers', 
-  'tomato sauce', 
-  'tomato paste', 
-  'crushed tomatoes', 
-  'ketchup', 
-  'black beans', 
-  'kidney beans', 
-  'pink beans', 
-  'rice', 
-  'pasta', 
-  'coffee', 
-  'cake mixes', 
-  'canned fruits', 
-  'canned vegetables', 
-  'tuna', 
-  'peanut butter', 
+  'white socks',
+  'mens underwear',
+  't-shirts',
+  'bars of soap',
+  'pants',
+  'long underwear',
+  'shoes',
+  'sweaters',
+  'jackets',
+  'shaving cream',
+  'razors',
+  'shampoo',
+  'conditioner',
+  'body wash',
+  'toothbrushes',
+  'toothpaste',
+  'lotion',
+  'hair brushes',
+  'nail clippers',
+  'tomato sauce',
+  'tomato paste',
+  'crushed tomatoes',
+  'ketchup',
+  'black beans',
+  'kidney beans',
+  'pink beans',
+  'rice',
+  'pasta',
+  'coffee',
+  'cake mixes',
+  'canned fruits',
+  'canned vegetables',
+  'tuna',
+  'peanut butter',
   'jelly'
 ]
 
 sarnelli_needs_list.each do | item |
-  Need.create(organization_id: sarnelli.id, item: item)
+  Need.create!(
+    organization_id: sarnelli.id,
+    item: item,
+    category: category
+  )
 end
