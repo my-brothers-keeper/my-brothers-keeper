ad4567ef873a1ddeb5b7ac061025d32674121674
Jordan Hollinger <jhollinger2@gmail.com>
2018-05-08 07:19:53 -0400

    Move map load into json file, and use ajax call to create popup from show organization view

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,83 @@
+function init_map(element_name) {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiamhvbGxpbmdlciIsImEiOiJjamdqc2gwemowbW5jMnhxOTM5Zmd0Zml6In0.dZcZikoQoM2lR_YGcxWgrg';
+    var map = new mapboxgl.Map({
+        container: element_name, // container id
+        style: 'mapbox://styles/mapbox/light-v9', // stylesheet location
+        center: [-75.153398,39.967893], // starting position [lng, lat]
+        zoom: 12 // starting zoom
+    });
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
+    // Add geolocate control to the map.
+    map.addControl(new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true
+    }));
+    return map;
+}
+function load_organization_layer(map, source_url) {
+    map.on('load', function () {
+        // Add organization geojson as a data source
+        map.addSource("organizations", {
+            type: "geojson",
+            data: source_url + ".geojson"
+        });
+        map.addLayer({
+            "id": "places",
+            "type": "symbol",
+            "source": "organizations",
+            "layout": {
+                "icon-image": "circle-15",
+                "icon-allow-overlap": true,
+                "text-optional": true,
+                "text-field": "{name}",
+                "text-font": ["Open Sans Bold", "Arial Unicode MS Bold"],
+                "text-size": 11,
+                "text-letter-spacing": 0.05,
+                "text-offset": [0, 0.9],
+                "text-anchor": "top",
+                "text-justify": "center",
+                "text-padding": 0.5
+            },
+            "paint": {
+                "text-color": "#202",
+                "text-halo-color": "#fff",
+                "text-halo-width": 2
+            }
+        });
+    });
+    // When a click event occurs on a feature in the places layer, open a popup at the
+    // location of the feature, with description HTML from its properties.
+    map.on('click', 'places', function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var id = e.features[0].id;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+        var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                new mapboxgl.Popup()
+                    .setLngLat(coordinates)
+                    .setHTML(this.responseText)
+                    .addTo(map);
+            }
+        };
+        xhttp.open("GET", source_url + '/' + id, true);
+        xhttp.send();
+    });
+    // Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'places', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'places', function () {
+        map.getCanvas().style.cursor = '';
+    });
+}
