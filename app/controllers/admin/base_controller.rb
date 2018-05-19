d36b0f33f6ff0e475940118d3a7a64c2b337ffb2
Michael Lamb <mflambjr@gmail.com>
2018-05-19 14:58:03 -0400

    Move before_action to Admin::BaseController

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,6 +3,16 @@
 module Admin
   # Base class for shared admin functionality
   class BaseController < ApplicationController
+    before_action :require_login
     layout 'admin'
+
+    private
+  
+    def require_login
+      unless logged_in?
+        flash[:alert] = 'You must be logged in to access this section'
+        redirect_to login_path
+      end
+    end
   end
 end
