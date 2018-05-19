886ae3fe22860464e8bb61ede57b27187104c088
Michael Lamb <mflambjr@gmail.com>
2018-05-19 00:12:25 -0400

    Admin Orgs controller grabs org by current_user's org_id

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class OrganizationsController < BaseController
     def show
-      @organization = Organization.find(params[:id])
+      @organization = Organization.find(current_user.organization_id)
 
       @needs = @organization.needs.order(:item).includes(:category)
     end
