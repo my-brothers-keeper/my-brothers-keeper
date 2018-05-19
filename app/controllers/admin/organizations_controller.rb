73efa27d2f13d3bee8eebecdfa8bbf4a5762f356
Michael Lamb <mflambjr@gmail.com>
2018-05-19 14:59:33 -0400

    Remove manual ID lookup from Admin/Orgs controller

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class OrganizationsController < BaseController
     def show
-      @organization = Organization.find(current_user.organization_id)
+      @organization = current_user.organization
 
       @needs = @organization.needs.order(:item).includes(:category)
     end
