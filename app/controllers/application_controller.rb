4eae9d00a3d0e132debfc9524fdb4ee41285f56a
Michael Lamb <mflambjr@gmail.com>
2018-05-19 00:13:48 -0400

    before filter to redirect unauthorized users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  before_action :require_login
+
+  private
+
+  def require_login 
+    unless logged_in?
+      flash[:alert] = "You must be logged in to access this section"
+      redirect_to login_path
+    end 
+  end
 end
